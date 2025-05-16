a46f464409ff5ccc57091a873540ba451fb21479
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-05-16 11:15:57 +0100

    create global variables for use with operate fn

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function add(a,b) {
+let operator;
+let num1; // better to use an array for numbers, but this is just to get started
+let num2;
+
+function add(a, b) {
   return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
   return a - b;
 }
 
-function multiply (a,b) {
+function multiply (a, b) {
   return a * b;
 }
 
-function divide (a,b) {
+function divide (a, b) {
   return a / b;
+}
+
+function operate (op, a, b) {
+  add(a,b);
 }
