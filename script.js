f12b6780252a9ed15d09d44d97cbd02a40a2ce81
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-05-20 10:33:51 +0100

    update display on digit click

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let operator;
 let num1; // better to use an array for numbers, but this is just to get started
 let num2;
 
+const display = document.querySelector(".display");
+
+digits = document.getElementsByClassName("digit");
+for (digit of digits) {
+  digit.addEventListener("click", getDigit);
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -21,4 +28,17 @@ function divide (a, b) {
 function operate (op, a, b) {
   const result = add(a,b);
   return result;
+}
+
+// returns (in the form of a string) the value of the digit clicked on
+function getDigit (event) {
+  number = event.target.innerHTML;
+  // alert(number);
+  updateDisplay(number);
+}
+
+// update display with value of the digit clicked on
+function updateDisplay(value) {
+  display.innerHTML = display.innerHTML + value;
+  // alert(Number(display.innerHTML))
 }
